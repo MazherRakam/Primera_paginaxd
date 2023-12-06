da2d9e5b78fec5910fda9bde68dda12cba860473
MazherRakam <megamiguel24@gmail.com>
2023-12-06 17:15:01 -0400

    Clase 6/12

---
+++ script.js
@@ -0,0 +1,13 @@
+let myVariable = 7
+let n = 45
+let c = "45"
+let velda=true
+let lista = [50, 77, 29, 100, 47, "Miguel", "Blanca"]
+let objeto= {
+    name: "Miguel",
+    email: "askldjalksdj@gmail.com",
+    pass: "1234"
+}
+
+console.log(objeto)
+console.log(n)
