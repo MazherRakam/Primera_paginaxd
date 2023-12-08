958145c7d5f51b7b3d1ed7a961d111aebb9fe83e
MazherRakam <megamiguel24@gmail.com>
2023-12-08 17:01:40 -0400

    Clase 08/12

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ let objeto= {
     email: "askldjalksdj@gmail.com",
     pass: "1234"
 }
+// operador ternario
+// console.log(
+//     4 > 6 ? "Se cumple" : "No se cumple"
+// )
+// Condicional
+// if(true) console.log("Se cumplio")
+// else console.log("No se cumplio")
 
-console.log(objeto)
-console.log(n)
+// Funcion normal
+// function EsPar(n){
+//     return (n % 2) ? "Impar" : "Par"
+// }
+// // Funcion tipo flecha
+// let EsParFlecha = (n) => n % 2 ? "Impar" : "Par"
+
+
+// console.log(EsParFlecha(7))
+
+
+// let Callback = ( ) => console.log("Se ejecuto el callback")
+// setTimeout( Callback, 5000 )
+
+axios.get("https://leonardoapi.onrender.com/music")
+    .then((res) => console.log(res.data))
